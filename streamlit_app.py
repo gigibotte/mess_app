4e698fe084aa4e74c140ec5443985fc5bf2317c4
gigibotte <gigi.bottecchia@gmail.com>
2023-08-07 21:40:35 +0200

    updated home

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,14 @@
 import streamlit as st
 
 # Set page configuration and add a title to the app
-st.set_page_config(
-    page_title="MESS",
-    page_icon="graphics\mess_logo.png",
-    layout="wide"
-)
+st.set_page_config(page_title="MESS Model", layout="wide")
+
+
 
 # Display the logo image in the header
-logo_path = "graphics\mess_logo.png"  # Path to your logo image file
-st.image(logo_path, use_column_width=True)
+#logo_path = "graphics\mess_logo.png"  # Path to your logo image file
+#st.image(logo_path, use_column_width=True)
 
 # Continue building the rest of your Streamlit app content below the header
 # For example, you can add text, input widgets, charts, etc.
-st.write("Welcome to your Streamlit app!")
+#st.write("Welcome to your Streamlit app!")
